181e74d30c8ec303dfeaa7192fd2d138e80fcd7f
Mikhail <mikhail@volkovlabs.io>
2023-01-02 19:16:41 -0500

    Add tests

--- src/components/EChartsPanel/EChartsPanel.test.tsx
+++ src/components/EChartsPanel/EChartsPanel.test.tsx
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import { toDataFrame } from '@grafana/data';
+import { LoadingState, toDataFrame } from '@grafana/data';
 import { EChartsPanel } from './EChartsPanel';
 
 /**
@@ -19,6 +19,44 @@ describe('Panel', () => {
   it('Should find component', async () => {
     const getComponent = ({ options = { name: 'data' }, ...restProps }: any) => {
       const data = {
+        series: [
+          toDataFrame({
+            name: 'data',
+            fields: [],
+          }),
+        ],
+      };
+      return <EChartsPanel data={data} {...restProps} options={options} />;
+    };
+
+    const wrapper = shallow(getComponent({}));
+    const div = wrapper.find('div');
+    expect(div.exists()).toBeTruthy();
+  });
+
+  it('Should find component with Done state', async () => {
+    const getComponent = ({ options = { name: 'data' }, ...restProps }: any) => {
+      const data = {
+        state: LoadingState.Done,
+        series: [
+          toDataFrame({
+            name: 'data',
+            fields: [],
+          }),
+        ],
+      };
+      return <EChartsPanel data={data} {...restProps} options={options} />;
+    };
+
+    const wrapper = shallow(getComponent({}));
+    const div = wrapper.find('div');
+    expect(div.exists()).toBeTruthy();
+  });
+
+  it('Should find component for Streaming', async () => {
+    const getComponent = ({ options = { name: 'data' }, ...restProps }: any) => {
+      const data = {
+        state: LoadingState.Streaming,
         series: [
           toDataFrame({
             name: 'data',
