8a63187cb94d297d7e29f88d664e492fb6d2e832
Mikhail Volkov <47795110+mikhail-vl@users.noreply.github.com>
2023-02-18 09:14:26 -0500

    Update CI and Release workflows (#134)
    * Update CI and Release workflows
    
    * Don't use Node 18
    
    * Update CI
    
    * Update Release workflow
    
    * Update CHANGELOG.md

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,6 @@ const standard = require('@grafana/toolkit/src/config/jest.plugin.config');
 // This process will use the same config that `yarn test` is using
 module.exports = {
   ...standard.jestConfig(),
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   collectCoverage: true,
 };
