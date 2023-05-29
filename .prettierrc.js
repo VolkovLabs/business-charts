cd47549b6c11f49a7f8f656c26d3a08d9591c885
Mikhail <mikhail@volkovlabs.io>
2023-05-29 17:52:09 -0400

    Migrate to Plugin Tools 1.5.0

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-  useTabs: false,
-  tabWidth: 2,
-  semi: true,
-  bracketSpacing: true,
-  arrowParens: 'always',
-  ...require('./node_modules/@grafana/toolkit/src/config/prettier.plugin.config.json'),
+  // Prettier configuration provided by Grafana scaffolding
+  ...require('./.config/.prettierrc.js'),
 };
