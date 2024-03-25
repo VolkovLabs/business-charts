ad9542448948d093dc83110b557b4e8a31657d8a
asimonok <sashasimonok@gmail.com>
2024-03-25 16:06:23 +0300

    Add migration for getOption code to replace non-context code parameters

--- src/migration.test.ts
+++ src/migration.test.ts
@@ -26,4 +26,122 @@ describe('Migrations', () => {
       });
     });
   });
+
+  describe('6.0.0', () => {
+    describe('getOption', () => {
+      it.each([
+        {
+          name: 'panel.data',
+          initial: `
+          const data = [];
+          data.series.forEach();
+          `,
+          expected: `
+          const data = [];
+          context.panel.data.series.forEach();
+          `,
+        },
+        {
+          name: 'grafana.replaceVariables',
+          initial: `
+          const replaceVariables = () => {};
+          replaceVariables('123')
+          `,
+          expected: `
+          const replaceVariables = () => {};
+          context.grafana.replaceVariables('123')
+          `,
+        },
+        {
+          name: 'grafana.theme',
+          initial: `
+          const theme = {};
+          theme.color;
+          `,
+          expected: `
+          const theme = {};
+          context.grafana.theme.color;
+          `,
+        },
+        {
+          name: 'panel.chart',
+          initial: `
+          const echartsInstance = {};
+          echartsInstance.color;
+          `,
+          expected: `
+          const echartsInstance = {};
+          context.panel.chart.color;
+          `,
+        },
+        {
+          name: 'echarts',
+          initial: `
+          const echarts = {};
+          echarts.color;
+          `,
+          expected: `
+          const echarts = {};
+          context.echarts.color;
+          `,
+        },
+        {
+          name: 'ecStat',
+          initial: `
+          const ecStat = {};
+          ecStat.color;
+          `,
+          expected: `
+          const ecStat = {};
+          context.ecStat.color;
+          `,
+        },
+        {
+          name: 'grafana.eventBus',
+          initial: `
+          eventBus.subscribe();
+          `,
+          expected: `
+          context.grafana.eventBus.subscribe();
+          `,
+        },
+        {
+          name: 'grafana.locationService',
+          initial: `
+          locationService.replace()
+          `,
+          expected: `
+          context.grafana.locationService.replace()
+          `,
+        },
+        {
+          name: 'grafana.notifySuccess',
+          initial: `
+          notifySuccess()
+          `,
+          expected: `
+          context.grafana.notifySuccess()
+          `,
+        },
+        {
+          name: 'grafana.notifyError',
+          initial: `
+          notifyError()
+          `,
+          expected: `
+          context.grafana.notifyError()
+          `,
+        },
+      ])('Should migrate $name', ({ initial, expected }) => {
+        expect(
+          getMigratedOptions({
+            pluginVersion: '5.2.0',
+            options: {
+              getOption: initial,
+            },
+          } as any).getOption
+        ).toEqual(expected);
+      });
+    });
+  });
 });
--- src/migration.ts
+++ src/migration.ts
@@ -1,4 +1,5 @@
 import { PanelModel } from '@grafana/data';
+import semver from 'semver';
 
 import { EditorMode } from './constants';
 import { PanelOptions } from './types';
@@ -14,6 +15,52 @@ interface OutdatedPanelOptions extends Omit<PanelOptions, 'editorMode'> {
 }
 
 /**
+ * Normalize Get Option
+ */
+const normalizeGetOption = (code: string): string => {
+  const search =
+    /(data.series|replaceVariables\(|theme.|echartsInstance.|echarts.|ecStat.|eventBus|locationService|notifySuccess\(|notifyError\()/gm;
+
+  return code.replace(search, (value) => {
+    switch (value) {
+      case 'data.series': {
+        return 'context.panel.data.series';
+      }
+      case 'replaceVariables(': {
+        return 'context.grafana.replaceVariables(';
+      }
+      case 'theme.': {
+        return 'context.grafana.theme.';
+      }
+      case 'echartsInstance.': {
+        return 'context.panel.chart.';
+      }
+      case 'echarts.': {
+        return 'context.echarts.';
+      }
+      case 'ecStat.': {
+        return 'context.ecStat.';
+      }
+      case 'eventBus': {
+        return 'context.grafana.eventBus';
+      }
+      case 'locationService': {
+        return 'context.grafana.locationService';
+      }
+      case 'notifySuccess(': {
+        return 'context.grafana.notifySuccess(';
+      }
+      case 'notifyError(': {
+        return 'context.grafana.notifyError(';
+      }
+      default: {
+        return value;
+      }
+    }
+  });
+};
+
+/**
  * Get Migrated Options
  *
  * @param panel
@@ -26,6 +73,13 @@ export const getMigratedOptions = (panel: PanelModel<OutdatedPanelOptions>): Pan
    */
   if (!options.editorMode) {
     options.editorMode = EditorMode.CODE;
+  }
+
+  /**
+   * Normalize non context code parameters before 6.0.0
+   */
+  if (panel.pluginVersion && semver.lt(panel.pluginVersion, '6.0.0')) {
+    options.getOption = normalizeGetOption(options.getOption);
   }
 
   return options;
