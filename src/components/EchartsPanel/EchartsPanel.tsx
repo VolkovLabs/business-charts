7d79f7931dd336efe51451508f3e2784ae10b7af
vitPinchuk <vit.pinchuck@gmail.com>
2024-09-11 11:58:59 +0300

    imports support updated

--- src/components/EchartsPanel/EchartsPanel.tsx
+++ src/components/EchartsPanel/EchartsPanel.tsx
@@ -152,126 +152,145 @@ export const EchartsPanel: React.FC<Props> = ({ options, data, width, height, re
     }
 
     /**
-     * Remove error
+     * Make available get async return from options editor
      */
-    setError(undefined);
-
-    /**
-     * Execution Function
-     */
-    try {
-      const func =
-        options.editorMode === EditorMode.VISUAL
-          ? new Function('context', options.visualEditor.code)
-          : new Function('context', options.getOption);
+    const runOptions = async () => {
+      /**
+       * Remove error
+       */
+      setError(undefined);
 
       /**
-       * Load Maps
+       * Execution Function
        */
-      switch (options.map) {
-        case Map.NONE:
-          break;
-        case Map.JSON:
-          registerMaps();
-          break;
-        case Map.GMAP:
-          loadGoogle(options.google);
-          break;
-        case Map.BMAP:
-          loadBaidu(options.baidu);
-          break;
-        case Map.AMAP:
-          loadGaode(options.gaode);
-          break;
-      }
-
-      /**
-       * Code Result
-       */
-      const contextPayload = {
-        grafana: {
-          theme,
-          replaceVariables,
-          eventBus,
-          locationService,
-          notifySuccess,
-          notifyError,
-          refresh: () => appEvents.publish({ type: 'variables-changed', payload: { refreshAll: true } }),
-        },
-        panel: {
-          data,
-          chart,
-        },
-        echarts,
-        ecStat,
-      };
-      const codeResult: CodeResult =
-        options.editorMode === EditorMode.VISUAL
-          ? func(
-              visualCodeParameters.create({
-                ...contextPayload,
-                editor: {
-                  dataset: {
-                    source: getDatasetSource(data.series, options.visualEditor.dataset),
-                  },
-                  series: getDataSeries(options.visualEditor.series, data.series, options.visualEditor.dataset),
-                  radar: getRadarOptions(options.visualEditor, data.series),
-                },
-              })
-            )
-          : func(codeParameters.create(contextPayload));
-
-      /**
-       * Chart option
-       */
-      let chartOption;
-
-      /**
-       * Default Option Config with merge disabled
-       */
-      let chartOptionConfig: echarts.EChartsOptionConfig = {
-        notMerge: true,
-      };
-
-      /**
-       * Check version
-       */
-      if (codeResult && 'version' in codeResult && codeResult.version === 2) {
-        /**
-         * Handle result v2
-         */
-        chartOption = codeResult.option || {};
-        chartOptionConfig = codeResult.config || chartOptionConfig;
+      try {
+        const func =
+          options.editorMode === EditorMode.VISUAL
+            ? new Function('context', options.visualEditor.code)
+            : new Function('context', options.getOption);
 
         /**
-         * Set Unsubscribe Function
+         * Load Maps
          */
-        const unsubscribeFunction = codeResult.unsubscribe;
-        if (typeof unsubscribeFunction === 'function') {
-          unsubscribeFn = () => {
-            unsubscribeFunction();
-          };
+        switch (options.map) {
+          case Map.NONE:
+            break;
+          case Map.JSON:
+            registerMaps();
+            break;
+          case Map.GMAP:
+            loadGoogle(options.google);
+            break;
+          case Map.BMAP:
+            loadBaidu(options.baidu);
+            break;
+          case Map.AMAP:
+            loadGaode(options.gaode);
+            break;
         }
-      } else {
-        /**
-         * Handle result v1
-         */
-        chartOption = codeResult || {};
-      }
 
-      /**
-       * Set Options
-       */
-      chart.setOption(
-        {
-          backgroundColor: 'transparent',
-          ...chartOption,
-        },
-        chartOptionConfig
-      );
-    } catch (err) {
-      setError(err instanceof Error ? err : new Error(`${err}`));
-    }
+        /**
+         * Code Result
+         */
+        const contextPayload = {
+          grafana: {
+            theme,
+            replaceVariables,
+            eventBus,
+            locationService,
+            notifySuccess,
+            notifyError,
+            refresh: () => appEvents.publish({ type: 'variables-changed', payload: { refreshAll: true } }),
+          },
+          panel: {
+            data,
+            chart,
+          },
+          echarts,
+          ecStat,
+        };
+
+        let codeResult;
+
+        const result: CodeResult =
+          options.editorMode === EditorMode.VISUAL
+            ? func(
+                visualCodeParameters.create({
+                  ...contextPayload,
+                  editor: {
+                    dataset: {
+                      source: getDatasetSource(data.series, options.visualEditor.dataset),
+                    },
+                    series: getDataSeries(options.visualEditor.series, data.series, options.visualEditor.dataset),
+                    radar: getRadarOptions(options.visualEditor, data.series),
+                  },
+                })
+              )
+            : func(codeParameters.create(contextPayload));
+
+        /**
+         * The result of the editor's execution
+         */
+        if (result instanceof Promise) {
+          codeResult = await result;
+        } else {
+          codeResult = result;
+        }
+
+        /**
+         * Chart option
+         */
+        let chartOption;
+
+        /**
+         * Default Option Config with merge disabled
+         */
+        let chartOptionConfig: echarts.EChartsOptionConfig = {
+          notMerge: true,
+        };
+
+        /**
+         * Check version
+         */
+        if (codeResult && 'version' in codeResult && codeResult.version === 2) {
+          /**
+           * Handle result v2
+           */
+          chartOption = codeResult.option || {};
+          chartOptionConfig = codeResult.config || chartOptionConfig;
+
+          /**
+           * Set Unsubscribe Function
+           */
+          const unsubscribeFunction = codeResult.unsubscribe;
+          if (typeof unsubscribeFunction === 'function') {
+            unsubscribeFn = () => {
+              unsubscribeFunction();
+            };
+          }
+        } else {
+          /**
+           * Handle result v1
+           */
+          chartOption = codeResult || {};
+        }
+
+        /**
+         * Set Options
+         */
+        chart.setOption(
+          {
+            backgroundColor: 'transparent',
+            ...chartOption,
+          },
+          chartOptionConfig
+        );
+      } catch (err) {
+        setError(err instanceof Error ? err : new Error(`${err}`));
+      }
+    };
+
+    runOptions();
 
     return unsubscribeFn;
 
